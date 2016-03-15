61be6213e2aad036cf0757a5cf9ac0fc3e527d04
EMPaThy789 <fyempathy@gmail.com>
2016-03-14 21:30:28 -0600

    Created basic DCEL/Half-edge Data structure
    data structure not actually being used yet, main.cpp is still assignmetn
    file

---
+++ DCEL.cpp
@@ -0,0 +1,13 @@
+#include "DCEL.h"
+
+using namespace std;
+
+// Implementation of doublely connected edge list (DCEL) aka half-edge data structure
+
+// constructor
+DCEL::DCEL()
+{
+
+}
+
+// Methods go here
---
+++ Vertex.cpp
@@ -0,0 +1,8 @@
+#include "Vertex.h"
+using namespace std;
+
+// Vertex Constructor
+Vertex::Vertex(float x, float y, float z, HalfEdge* he, float nx, float ny, float nz)
+{
+
+}
