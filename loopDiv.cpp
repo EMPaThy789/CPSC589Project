d826fb6c88361fb5b39421dffac070545c8f3f5d
Kiranpreetsb <kiran_s_b@hotmail.com>
2016-03-29 17:30:37 -0600

    Created two new files for loop subdivision (may not need the header file) in the .cpp file, added an outline of how to implement loop subdivision (from class notes)

---
+++ loopDiv.cpp
@@ -0,0 +1,20 @@
+//
+//  loopDiv.cpp
+//  CPSC589Project
+//
+//  Created by Kiranpreet Bajwa on 2016-03-29.
+//  Copyright © 2016 Kiranpreet Bajwa. All rights reserved.
+//
+
+#include "loopDiv.h"
+
+//Steps of subdividing
+
+//1. Copy the vertices
+
+//2. Find the neighbours and alpha if dealing with extra-ordinary vertex
+
+//3. For all edges, insert u (new vertex)
+
+//4. For all faces, find the new and old vertices, add the new faces to f' (?), add the new edges to vi
+        //and add the old vertices to the edges.
