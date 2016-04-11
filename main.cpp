80d3519ea8780dcb5a1468a6e50659ceeb4df1c2
adjuric <acodjuric@hotmail.com>
2016-04-10 19:22:10 -0600

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ Kiranpreet Bajwa
 Aleks Djuric
 */
 #include <GLFW/glfw3.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <iostream>
 #include <math.h>
 #include <vector>
@@ -24,11 +26,12 @@ struct Point
 
 GLFWwindow *window;
 int w, h;
-float x, y, z, rY, rX;
+float x, y, z, rY, rX,lx,ly,lz =0;
 
 double mouseX, mouseY;
 const int WINDOW_HEIGHT = 600,
           WINDOW_WIDTH = 800;
+          Vertex* somePoint;
 
 DCEL dcel;
 
@@ -47,6 +50,33 @@ void keyboard(GLFWwindow *sender, int key, int scancode, int action, int mods)
 	{
 		z += 0.1;
 	}
+	
+	//LIGHT CONTROLS
+	if (key == GLFW_KEY_U && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		lx -= 0.4;
+	}
+	if (key == GLFW_KEY_J && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		lx += 0.4;
+	}
+	if (key == GLFW_KEY_I && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		ly -= 0.4;
+	}
+	if (key == GLFW_KEY_K && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		ly -= 0.4;
+	}
+	
+	if (key == GLFW_KEY_O && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		lz += 0.4;
+	}if (key == GLFW_KEY_L && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		lz -= 0.4;
+	}
+
 	/* these seem a bit rekt atm so dont use
 	if (key == GLFW_KEY_A && (action == GLFW_PRESS || action == GLFW_REPEAT))
 	{
@@ -82,22 +112,88 @@ void keyboard(GLFWwindow *sender, int key, int scancode, int action, int mods)
 	{
 		rX += 2;
 	}
-
 	
 	
-}
-
-
-
-void mouseClick(GLFWwindow *sender, int button, int action, int mods)
-{
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	
+	if (key == GLFW_KEY_KP_7 && (action == GLFW_PRESS || action == GLFW_REPEAT))
 	{
+		somePoint->x -= 1;
 	}
-
-
+	if (key == GLFW_KEY_KP_4 && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		somePoint->x +=1;
+	}
+	if (key == GLFW_KEY_KP_8 && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		somePoint->y -= 1;
+	}
+	if (key == GLFW_KEY_KP_5 && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		somePoint->y +=1;
+	}
+		if (key == GLFW_KEY_KP_9 && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		somePoint->z -= 1;
+	}
+	if (key == GLFW_KEY_KP_6 && (action == GLFW_PRESS || action == GLFW_REPEAT))
+	{
+		somePoint->z +=1;
+	}
 	
 }
+
+
+
+void mouseClick (GLFWwindow *sender, int button, int action, int mods) {
+  
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS){               
+        
+        GLint viewport[4]; 
+        glGetIntegerv( GL_VIEWPORT, viewport ); 
+        GLdouble modelview[16]; 
+        glGetDoublev( GL_MODELVIEW_MATRIX, modelview ); 
+        GLdouble projection[16]; 
+        glGetDoublev( GL_PROJECTION_MATRIX, projection );
+        GLfloat winX, winY, winZ; 
+        
+        GLdouble worldX, worldY, worldZ;         
+        winX = (float)mouseX;
+        winY = (float)viewport[3] - (float)mouseY;
+        glReadPixels(mouseX, int(winY),1,1,GL_DEPTH_COMPONENT,GL_FLOAT,&winZ);
+        
+        
+        GLdouble vX, vY, vZ;  
+         
+        //World coordinates from the given screen coordinates
+     //   gluUnProject( winX, winY, winZ, modelview, projection, viewport, &worldX, &worldY, &worldZ);
+               
+               cout << "WINX IS: " << winX << endl;
+               cout << "WINY is: " << winY << endl;
+               cout << "WINZ is : " << winZ << endl;       
+
+               
+               for(int i=0; i < dcel.vertexList.size();i++){
+			   gluProject(dcel.vertexList[i]->x, dcel.vertexList[i]->y, dcel.vertexList[i]->z,
+							modelview,projection,viewport, &vX, &vY, &vZ);
+
+			   
+			   if((winX - vX) <= 5 && (winX - vX) >= -5 ){
+					if((winY -vY) <= 5 && (winY -vY) >= -5){
+						cout << "FOUND YOU" << endl;
+				cout << "vX : " << vX << endl;
+			   cout << "vY : " << vY << endl;
+			   cout << "Vertex Z : " << dcel.vertexList[i]->z << endl;
+			   cout << "vZ : :" << vZ << endl;
+			   somePoint = dcel.vertexList[i];
+			   break;
+					}
+			   }
+			   
+			   }
+    }
+}
+
+
 
 
 void mousePos(GLFWwindow *sender, double x, double y)
@@ -110,6 +206,7 @@ void render()
 {
 	glEnable(GL_DEPTH_TEST);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
 
 	//Functions for changing transformation matrix
 	glMatrixMode(GL_MODELVIEW);
@@ -120,6 +217,19 @@ void render()
 	glRotatef(rY, 0.0f, 1.0f, 0.0f);
 	glRotatef(rX, 1.0f, 0.0f, 0.0f);
 	//glScalef(0.5f, 0.5f, 0.5f);
+	
+	
+	GLfloat light0_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light0_position[] = { 0.0+lx, -1.0+ly, -1.0+lz, 0.0 };
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	//glColorMaterial(GL_FRONT, GL_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_NORMALIZE);
 
 	//Functions for changing projection mdatrix
 	glMatrixMode(GL_PROJECTION);
@@ -193,7 +303,6 @@ int main(int argc, char* argv[])
 	z = -10;
 	rX = 0;
 	rY = 0;
-	
 
 
 	if (!glfwInit())
@@ -208,19 +317,8 @@ int main(int argc, char* argv[])
 	glfwSetCursorPosCallback(window, mousePos);
 
 
-	GLfloat light0_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat light0_position[] = { 0.0, -1.0, -1.0, 0.0 };
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glShadeModel(GL_SMOOTH);
-	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glColorMaterial(GL_FRONT, GL_DIFFUSE);
-	glEnable(GL_COLOR_MATERIAL);
-	glEnable(GL_NORMALIZE);
 
-	dcel.readOBJ("cube.obj");
+	dcel.readOBJ("cad.obj");
 	
 	while (!glfwWindowShouldClose(window)) {
 		glfwGetFramebufferSize(window, &w, &h);
@@ -230,6 +328,9 @@ int main(int argc, char* argv[])
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+		
+		
+		
 	}
 
 	glfwDestroyWindow(window);
